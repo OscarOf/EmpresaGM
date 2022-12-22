ced879237ed8637c2a33267210be510f5bfa6f6d
Óscar O <oscarforjuela@hotmail.com>
2022-12-22 16:51:01 -0500

    New Add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import imagenes from './Images/imagenes';
+//import Title from './Components/Title';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={imagenes.logo} alt="No se ha encontrado la imagen"></img>
+      <button className='btn btn-primary'>Hola</button>
+      
     </div>
   );
 }
---
+++ src/Components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import titulo from '../Images/Logo.png';
+
+const Title = ()=>{
+    return(
+        <img src={titulo} alt='No se ha encontrado la imagen'/>
+    )
+}
+
+export default Title;
