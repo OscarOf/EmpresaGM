803b2c5ff42b1dc97a9a26b5c85feef66087f81f
Óscar O <oscarforjuela@hotmail.com>
2022-12-30 19:12:22 -0500

    New Router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import {router} from './router';
-import {HashRouter, RouterProvider} from 'react-router-dom'
+import {RouterProvider} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter router={router}/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import CarShop from "../Pages/CarShop";
 import Home from "../Pages/Home";
 import Services from "../Pages/Services";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/",
         element: <Home />
