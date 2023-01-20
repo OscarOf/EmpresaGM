e21239cb4e42edfd7fd2963b62cc5b5991b5ea48
Óscar O <oscarforjuela@hotmail.com>
2023-01-19 23:31:20 -0500

    New Images and Carousel

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,46 +1,34 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
 import imagenes from "../Images/imagenes";
 
 const Slider = () => {
 
     return (
-        <div id="carouselExampleCaptions" className="carousel slide">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={imagenes.Imag1} className="d-block activo" alt="No se ha encontrado la imagen"/>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5 className="text-dark">Aire Acondicionado</h5>
-                            <p className="text-dark">Se otorga mantenimiento preventivo y de corrección, venta y suministro.</p>
-                        </div>
-                </div>
-                <div className="carousel-item">
-                    <img src={imagenes.Imag2} className="d-block" alt="No se ha encontrado la imagen"/>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Equipos de Bombeo</h5>
-                            <p>Mantenimiento de Motobombas, hidroflops, automatización</p>
-                        </div>
-                </div>
-                <div className="carousel-item">
-                    <img src={imagenes.Imag3} className="d-block" alt="No se ha encontrado la imagen"/>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Equipos Industriales</h5>
-                            <p>Se otorga mantenimiento preventivo y de corrección, venta y suministro.</p>
-                        </div>
-                </div>
-            </div>
-            <button className="carousel-control-prev bg-dark" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next bg-dark" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+        <div className="container">
+            <Carousel>
+                <Carousel.Item>
+                    <img className="d-block w-80" src={imagenes.Imag1} alt="La imagen no se encuentra disponible" />
+                    <Carousel.Caption>
+                        <h3 className="text-dark">Aire Acondicionado</h3>
+                        <p className="text-dark">Se otorga mantenimiento preventivo y de corrección, venta y suministro.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="d-block w-80" src={imagenes.Imag1} alt="La imagen no se encuentra disponible" />
+                    <Carousel.Caption>
+                        <h3 className="text-dark">Equipos de Bombeo</h3>
+                        <p className="text-dark">Mantenimiento de Motobombas, hidroflops, automatización</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="d-block w-80" src={imagenes.Imag1} alt="La imagen no se encuentra disponible" />
+                    <Carousel.Caption>
+                        <h3 className="text-dark">Equipos Industriales</h3>
+                        <p className="text-dark">Se otorga mantenimiento preventivo y de corrección, venta y suministro.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
--- src/Images/imagenes.js
+++ src/Images/imagenes.js
@@ -1,11 +1,17 @@
 import Logo from './Logo.png';
 import Imag1 from './imagen1.png';
-import Imag2 from './imagen2.png';
+import Imag2 from './imagen2.jpg';
 import Imag3 from './imagen3.png';
+import Imag4 from './imagen4.jpg';
+import Imag5 from './imagen5.jpg';
+import Imag6 from './imagen6.jpg';
 
 export default {
     "logo": Logo,
     "Imag1": Imag1,
     "Imag2": Imag2,
-    "Imag3": Imag3,    
+    "Imag3": Imag3,
+    "Imag4": Imag4,
+    "Imag5": Imag5,
+    "Imag6": Imag6
 }
